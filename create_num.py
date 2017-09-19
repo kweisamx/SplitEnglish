aff098bfd1481b6f8c699a519916afa41d723529
kweisamx <kweisamx0322@gmail.com>
2017-09-19 10:57:05 +0800

    the first image split is finish

--- create_num.py
+++ create_num.py
@@ -5,36 +5,54 @@ from random import randint
 #CONST
 height = 130
 width = 65
-number = 4
-
+number = 3
+english_num= 3
+english_map = dict(zip((0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25),('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')))
 
 def Create_blank(width, height, rgb_color=(0, 0, 0)):
-	"""Create new image(numpy array) filled with certain color in RGB"""
-	# Create black blank image
-	image = np.zeros((height, width, 3), np.uint8)
+    """Create new image(numpy array) filled with certain color in RGB"""
+    # Create black blank image
+    image = np.zeros((height, width, 3), np.uint8)
 
-	# Since OpenCV uses BGR, convert the color first
-	color = tuple(reversed(rgb_color))
-	# Fill image with color
-	image[:] = color
+    # Since OpenCV uses BGR, convert the color first
+    color = tuple(reversed(rgb_color))
+    # Fill image with color
+    image[:] = color
 
-	return image
+    return image
 
-def GetRandNum(nunber):
+def GetRandNum(nunber,digist=True):
     randnum = []
-    for i in range(number):
-	    randnum.append(str(randint(0,9)))
-    return randnum
+    if digist:
+        for i in range(number):
+            randnum.append(str(randint(0,9)))
+        return randnum
+    #english
+    else:
+        for i in range(number):
+            randnum.append(english_map[randint(0,25)])
+        return randnum
 
-def CombineImage(blank,element):
+
+
+def CombineImage(blank,element,name):
     for i in range(len(element)):
         blank[:height , i*width:(i+1) * width ,:3] = cv2.imread(element[i]+'.jpg')
-    cv2.imshow('all',blank)
+    cv2.imshow(name,blank)
     return blank
+
+def CombineTwoImage(img1,img2):
+    h1,w1,_ = img1.shape
+    h2,w2,_ = img2.shape
+    blank_image =  Create_blank(w1+w2,h1)
+    blank_image[:h1,:w1,:3] = img1
+    blank_image[:h2,w1:w1+w2,:3] = img2
+    cv2.imshow("CombineTwo",blank_image)
+    return blank_image
 
 def ClearCombineImage(img):
     gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY) # change the image to gray image
-    ret,binary = cv2.threshold(gray,127,255,cv2.THRESH_BINARY) # change the image to black and write
+    ret,binary = cv2.threshold(gray,200,255,cv2.THRESH_BINARY) # change the image to black and write
 
     _ , contours, hierarchy  =  cv2.findContours(binary,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)
     cv2.drawContours(img,contours,-1,(0,0,255),3)  
@@ -45,12 +63,18 @@ def ClearCombineImage(img):
 
 
 n = GetRandNum(number)
-print(n)
-
+e_n = GetRandNum(english_num,False)# not digits
+print(n,e_n)
 blankimg = Create_blank(width * number , height)
+blankimg_en = Create_blank(width * english_num , height)
+blankimg_all = Create_blank(width * (number + english_num),height)
 
 cv2.imshow("all",blankimg)
-img = CombineImage(blankimg,n)
+img = CombineImage(blankimg,n,"digits")
+img_en = CombineImage(blankimg_en,e_n,"english")
+
+CombineTwoImage(img,img_en)
+
 
 #ClearCombineImage(img)
 cv2.waitKey(0) 
--- split.py
+++ split.py
@@ -4,7 +4,7 @@ import numpy as np
 def Cut(width_start,width,height_start,height,interval,alph,img):
 	for index ,element in enumerate(alph):
 		split_img = img[height_start:height_start + height , width_start : width + width_start]
-		#cv2.imshow(alph[index],split_img)
+		cv2.imshow(alph[index],split_img)
 		width_start = width_start + width + interval
 		cv2.imwrite(alph[index]+".jpg", split_img)
 		#cv2.waitKey(0)
@@ -12,41 +12,41 @@ def Cut(width_start,width,height_start,height,interval,alph,img):
 img = cv2.imread('d.png')
 img2 = cv2.imread('e.png')
 cv2.imshow("all",img)
-cv2.imshow("all2",img2)
+#cv2.imshow("all2",img2)
 
 # the 1 layer
-w1_start = 75
-w1 = 65
-h1_start = 0
-h1 = 130
+w1_start = 81
+w1 = 56
+h1_start = 13
+h1 = 107
 alph1 = ['a','b','c','d','e']
 
 # the 2 layer
-w2_start = 75
-w2 = 65
-h2_start = 190
-h2 = 130
+w2_start = 81
+w2 = 56
+h2_start = 209
+h2 = 107
 alph2 = ['f','g','h','i','j']
 
 # the 3 layer
-w3_start = 85
-w3 = 65
-h3_start = 380
-h3 = 130
+w3_start = 87
+w3 = 57
+h3_start = 384
+h3 = 107
 alph3 = ['k','l','m','n','o']
 
 # the 4 layer
-w4_start = 85
-w4 = 65
-h4_start = 570
-h4 = 130
+w4_start = 87
+w4 = 57
+h4_start = 582
+h4 = 107
 alph4 = ['p','q','r','s','t']
 
 # the 5 layer
 
 w5_start = 65
 w5 = 65
-h5_start = 35
+h5_start = 42
 h5 = 130
 alph5 = ['u','v','w','x','y']
 
@@ -54,7 +54,7 @@ alph5 = ['u','v','w','x','y']
 
 w6_start = 65
 w6 = 65
-h6_start = 230
+h6_start = 237
 h6 = 130
 alph6 = ['z']
 
@@ -62,7 +62,7 @@ alph6 = ['z']
 
 w7_start = 55
 w7 = 65
-h7_start = 410
+h7_start = 405
 h7 = 130
 alph7 = ['1','2','3','4','5']
 
@@ -75,12 +75,12 @@ h8 = 130
 alph8 = ['6','7','8','9','0']
 
 
-Cut(w1_start,w1,h1_start,h1,62,alph1,img)
-Cut(w2_start,w2,h2_start,h2,62,alph2,img)
-Cut(w3_start,w3,h3_start,h3,62,alph3,img)
-Cut(w4_start,w4,h4_start,h4,62,alph4,img)
-Cut(w5_start,w5,h5_start,h5,63,alph5,img2)
-Cut(w6_start,w6,h6_start,h6,63,alph6,img2)
-Cut(w7_start,w7,h7_start,h7,61,alph7,img2)
-Cut(w8_start,w8,h8_start,h8,61,alph8,img2)
+#Cut(w1_start,w1,h1_start,h1,70,alph1,img)
+#Cut(w2_start,w2,h2_start,h2,70,alph2,img)
+#Cut(w3_start,w3,h3_start,h3,71,alph3,img)
+Cut(w4_start,w4,h4_start,h4,71,alph4,img)
+#Cut(w5_start,w5,h5_start,h5,63,alph5,img2)
+#Cut(w6_start,w6,h6_start,h6,63,alph6,img2)
+#Cut(w7_start,w7,h7_start,h7,61,alph7,img2)
+#Cut(w8_start,w8,h8_start,h8,61,alph8,img2)
 cv2.waitKey(0)
