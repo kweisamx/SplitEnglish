4b718d170446579151ef3e99753d4afe3115d10c
kweisamx <kweisamx0322@gmail.com>
2017-09-25 20:52:24 +0800

    add the resize and fix the bug

--- create_num.py
+++ create_num.py
@@ -5,9 +5,10 @@ from random import randint
 #CONST
 height = 107
 width = 57
-number = 3
+number = 4
 english_num= 3
 english_map = dict(zip((0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25),('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')))
+resize_zoom = 1.65
 
 def Create_blank(width, height, rgb_color=(255, 255, 255)):
     """Create new image(numpy array) filled with certain color in RGB"""
@@ -21,7 +22,7 @@ def Create_blank(width, height, rgb_color=(255, 255, 255)):
 
     return image
 
-def GetRandNum(nunber,digist=True):
+def GetRandNum(number,digist=True):
     randnum = []
     if digist:
         for i in range(number):
@@ -36,10 +37,15 @@ def GetRandNum(nunber,digist=True):
 
 
 def CombineImage(blank,element,name):
+    w1_start = 0
     for i in range(len(element)):
         element_img = cv2.imread(element[i]+'.jpg')
         h1,w1,_ = element_img.shape
-        blank[:h1 , i*w1:(i+1) * w1 ,:3] = element_img
+        blank[:h1 , w1_start : w1_start + w1 ,:3] = element_img
+        w1_start = w1_start +w1
+        if i != len(element) - 1 :
+            blank[:h1 , w1_start:w1_start + 4,:3] = Create_blank(4,h1)
+            w1_start = w1_start + 4
     cv2.imshow(name,blank)
     return blank
 
@@ -60,24 +66,40 @@ def ClearCombineImage(img):
  #   print(len(contours))
  #   cv2.drawContours(img,contours,-1,(0,0,0),1)  
     cv2.imshow("img", binary)  
-    cv2.waitKey(0)  
+    cv2.waitKey(0) 
 
+
+def resize(img, width=None, height=None, inter=cv2.INTER_AREA):
+    dim = None
+    (h,w) = img.shape[:2]
+    if width == None:
+        r = height/float(h)
+        dim = (int(w * r), height)
+    else:
+        r= width / float(w)
+        dim = (width, int(h * r))
+    resized = cv2.resize(img, dim, interpolation=inter)
+    return resized
 
 
 
 n = GetRandNum(number)
 e_n = GetRandNum(english_num,False)# not digits
 print(n,e_n)
-blankimg = Create_blank(width * number , height)
-blankimg_en = Create_blank(width * english_num , height)
-blankimg_all = Create_blank(width * (number + english_num),height)
+blankimg = Create_blank(width * number + (number-1) * 4 , height)
+blankimg_en = Create_blank(width * english_num + (english_num-1) * 4 , height)
+blankimg_all = Create_blank(width * (number + english_num) +(number+ english_num - 2) * 4 ,height)
 
 cv2.imshow("all",blankimg)
 img = CombineImage(blankimg,n,"digits")
 img_en = CombineImage(blankimg_en,e_n,"english")
 
 combine_img = CombineTwoImage(img,img_en)
+(h,w) = combine_img.shape[:2]
 
+#resize_img = resize(combine_img,width = combine_img.shape[1] * 2, inter=cv2.INTER_AREA)
+resize_img = cv2.resize(combine_img, (int(w * resize_zoom) ,int( h * resize_zoom) ), interpolation=cv2.INTER_CUBIC)
+cv2.imshow("resize",resize_img)
 
 #ClearCombineImage(combine_img)
 cv2.waitKey(0) 
