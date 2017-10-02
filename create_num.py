60a2d6ec2675e2e67a2a5842d316da88368bddb3
kweisamx <kweisamx0322@gmail.com>
2017-10-02 10:11:00 +0800

    fix the rotate and zoom size

--- create_num.py
+++ create_num.py
@@ -8,7 +8,7 @@ width = 57
 number = 4
 english_num= 3
 english_map = dict(zip((0,1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20,21,22,23,24,25),('a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z')))
-resize_zoom = 1.65
+resize_zoom = 0.15
 
 def Create_blank(width, height, rgb_color=(255, 255, 255)):
     """Create new image(numpy array) filled with certain color in RGB"""
@@ -89,11 +89,11 @@ def AffineImage(img,name):
     cv2.imshow(name,res)
 def PerspectiveImage(img,name):
     img1 = np.float32([[0,0],[0,165],[650,0],[650,165]])
-    img2 = np.float32([[0,0],[0,165],[650,25],[650,125]])
+    img2 = np.float32([[0,0],[0,165],[600,25],[600,125]])
     h , mask = cv2.findHomography(img1,img2)
     out = cv2.warpPerspective(img,h,(800,800))
     cv2.imshow(name,out)
-
+   
 n = GetRandNum(number)
 e_n = GetRandNum(english_num,False)# not digits
 print(n,e_n)
@@ -109,10 +109,12 @@ combine_img = CombineTwoImage(img,img_en)
 (h,w) = combine_img.shape[:2]
 
 #resize_img = resize(combine_img,width = combine_img.shape[1] * 2, inter=cv2.INTER_AREA)
-resize_img = cv2.resize(combine_img, (int(w * resize_zoom) ,int( h * resize_zoom)-10 ), interpolation=cv2.INTER_CUBIC)
+print(w,w * resize_zoom ,h, h*resize_zoom)
+resize_img = cv2.resize(combine_img, (int(w * resize_zoom) ,int( h * resize_zoom) ), interpolation=cv2.INTER_AREA)
 cv2.imshow("resize",resize_img)
+cv2.imwrite("test.jpg",resize_img)
 #AffineImage(resize_img,"affine")
-PerspectiveImage(resize_img,"P_img")
+#PerspectiveImage(resize_img,"P_img")
 #ClearCombineImage(combine_img)
 cv2.waitKey(0) 
 
